23c388fe84e107afeaa68a57fedd19e31ee4da38
DNG-hub <73457626+DNG-hub@users.noreply.github.com>
2024-07-24 15:43:15 -0700

    What we Do page rough outline completed. Localizations added for navbar and buttons, What we do added to navbar

---
+++ Pages/WhatWeDo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InnovatorHome.Pages
+{
+    public class WhatWeDoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
