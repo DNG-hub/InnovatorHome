f9777493e461bfda6e7d88d3b1f991830e09f782
DNG-hub <73457626+DNG-hub@users.noreply.github.com>
2024-07-25 16:42:02 -0700

    emplemented a working Consulting page. will continue with localization

---
+++ Pages/Consulting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InnovatorHome.Pages
+{
+    public class ConsultingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
