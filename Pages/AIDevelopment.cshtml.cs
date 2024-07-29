dfef258b0cfe119cf5756a7a61de5354416f0d84
DNG-hub <73457626+DNG-hub@users.noreply.github.com>
2024-07-29 13:40:16 -0700

    AIDevelpment completed wit localization

---
+++ Pages/AIDevelopment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InnovatorHome.Pages
+{
+    public class AIDevelopmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
